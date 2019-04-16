83d90afefa5769ac2a726fca60b20b8dcfbd212a
kahlua <lucasclaude3@gmail.com>
2019-04-16 23:25:17 +0200

    Use airbnb code style

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-import { Drug, Pharmacy } from "./pharmacy";
-
-import fs from "fs";
+import fs from 'fs';
+import { Drug, Pharmacy } from './pharmacy';
 
 const drugs = [
-  new Drug("Doliprane", 20, 30),
-  new Drug("Herbal Tea", 10, 5),
-  new Drug("Fervex", 5, 40),
-  new Drug("Magic Pill", 15, 40)
+  new Drug('Doliprane', 20, 30),
+  new Drug('Herbal Tea', 10, 5),
+  new Drug('Fervex', 5, 40),
+  new Drug('Magic Pill', 15, 40),
 ];
 const trial = new Pharmacy(drugs);
 
 const log = [];
 
-for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
+for (let elapsedDays = 0; elapsedDays < 30; elapsedDays += 1) {
   log.push(JSON.stringify(trial.updateBenefitValue()));
 }
 
 /* eslint-disable no-console */
-fs.writeFile("output.txt", log, err => {
+fs.writeFile('output.txt', log, (err) => {
   if (err) {
-    console.log("error");
+    console.log('error');
   } else {
-    console.log("success");
+    console.log('success');
   }
 });
 /* eslint-enable no-console */
